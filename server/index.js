52211bb9aed635957f92f79dddf94de9e612452b
Erich Menezes <emenezes@thoughtworks.com>
2020-03-19 21:34:53 -0300

    :sparkles: creates endpoint to fetch individual edital

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const EDITAIS_FOLDER = __dirname + '/editais';
 
 app.get('/editais', async (_, res) => {
   const editais = fs.readdirSync(EDITAIS_FOLDER);
-  res.send(editais);
+  res.json(editais);
+});
+
+app.get('/editais/:edital', async (req, res) => {
+  const { edital } = req.params;
+  const editalFile = fs.readFileSync(`${EDITAIS_FOLDER}/${edital}`, 'UTF8');
+  res.send(editalFile);
 });
 
 app.use(express.static('build'));
