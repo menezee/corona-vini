a78ff102fed6f2d8d02697328d4f6fe06f4f1542
Erich Menezes <emenezes@thoughtworks.com>
2020-03-19 22:45:19 -0300

    :sparkles: accordion

--- src/app.js
+++ src/app.js
@@ -1,54 +1,54 @@
-import React, { lazy, useEffect, useState, Suspense } from 'react';
-const ReactMarkdown = lazy(() => import('react-markdown'));
+import React, { useEffect, useState} from 'react'
+import {Accordion, Icon} from 'semantic-ui-react'
+import ReactMarkdown from 'react-markdown';
 
-function Fallback() {
-  return (
-    <div>
-      Carregando edital...
-    </div>
-  )
-}
+export default function () {
+  const EDITAIS_BASE_URL = 'https://api.github.com/repos/Suprimentos-Covid-19-BR/SiteAgregador/contents';
+  const [editalLinks, setEditalLinks] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [editalMap, setEditalMap] = useState(new Map());
 
-function App() {
-  const EDITAIS_BASE_URL = '/editais';
-  const [listOfEditais, setListOfEditais] = useState([]);
-
-  const fetchListOfEditais = async () => await ((await fetch(EDITAIS_BASE_URL)).json());
-
-  const fetchEdital = async editais => {
-    const requestPromises =
-      await Promise
-        .allSettled(
-          editais.map(link => fetch(`${EDITAIS_BASE_URL}/${link}`))
-        )
-        .then(rawPromises => rawPromises.map(({value}) => value.text()));
-
-      return Promise
-        .allSettled(requestPromises)
-        .then(quotePromises => quotePromises.map(({value}) => value));
-  };
+  const fetchListOfEditais = async () => (await ((await fetch(EDITAIS_BASE_URL)).json())).map(({ download_url }) => download_url);
 
   useEffect(() => {
     (async () => {
-      const listOfLinks = await fetchListOfEditais();
-
-      setListOfEditais(
-        await fetchEdital(listOfLinks),
+      setEditalLinks(
+        await fetchListOfEditais(),
       );
     })();
   }, []);
 
+  const handleClick = async (e, titleProps) => {
+    const { index } = titleProps;
+    const newIndex = activeIndex === index ? -1 : index;
+    const markdown = editalMap.get(index) ?? await ((await fetch(editalLinks[index])).text());
+
+    setEditalMap(m => (
+      new Map([...m.entries(), [index, markdown]])
+    ));
+
+    setActiveIndex(newIndex);
+  };
+
   return (
-    <>
+    <Accordion fluid styled>
       {
-        listOfEditais.map((edital, i) => (
-          <Suspense fallback={<Fallback />}>
-            <ReactMarkdown source={edital} />
-          </Suspense>
+        editalLinks.map((link, i) => (
+          <>
+            <Accordion.Title
+              active={activeIndex === i}
+              index={i}
+              onClick={handleClick}
+            >
+              <Icon name='dropdown'/>
+              {link}
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === i}>
+              <ReactMarkdown source={editalMap.get(i)} />
+            </Accordion.Content>
+          </>
         ))
       }
-    </>
-  );
+    </Accordion>
+  )
 }
-
-export default App;
